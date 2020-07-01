7f00e748c63bf888bf937aacde43a9cbe09793b0
Anuj Talwar <66703039+alooperalta@users.noreply.github.com>
2020-07-01 23:12:40 +0530

    Add files via upload
    added the search bar, and did some detailing

---
+++ index.js
@@ -0,0 +1,6 @@
+// This is to toggle the category search bar
+/*  $(".search-icon").click(function(){
+  $(".search").toggleClass("hidden");
+  $(".search-bar").toggleClass("hidden");
+});*/
+// This is to toggle the category search bar
