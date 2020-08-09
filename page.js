9d162f0f88aaf3569e926d5ed9f23f9bbd2425c8
Harsh Goyal <harsh.student99@gmail.com>
2020-08-09 16:41:40 +0530

    Minor changes

--- page.js
+++ page.js
@@ -7,15 +7,26 @@ document.addEventListener("DOMContentLoaded", function(){
    rootRef.on("value", function(data){
        var mem= data.val();
        var keys = Object.keys(mem);
+       var count =0;
       for(var i=0;i<keys.length;i++)
       {
           var k= keys[i];
           var name = mem[k].brand;
-          if(name==brandSearch)
+          if(name===brandSearch)
           {
                 var bran= document.getElementById("brands");
                 bran.innerHTML= "<h1>"+name+"</h1>\
                   <p>"+mem[k].description+"</p>"
+          }
+          else
+          { console.log(name);
+             var alt= document.getElementById("alter");
+            alt.innerHTML += "<div class="+ "brand1"+ ">\
+                <div class="+"subbrand-loc"+">\
+                <h3><strong><a href="+mem[k].link+">"+name+"</a></strong></h3>\
+                <p class="+"answer"+">"+mem[k].description+"</p>\
+              </div>\
+            </div>"
           }
       }
  });
