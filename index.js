5302b88f6646ff2ecd72f411256917e1c204f2e4
Harsh Goyal <harsh.student99@gmail.com>
2020-08-14 23:39:09 +0530

    minor bug fixes Some designing required

--- index.js
+++ index.js
@@ -6,38 +6,42 @@
 // This is to toggle the category search bar
 
 
-// Object of brand and categroies
+//************************************************* Object of brand and categroies*****************************************
 var categor={
   "categories": ["TV", "Mobile","Headphones","Laptop","Washing Machine","2-wheeler","4-wheeler",
                   "Camera","Fan/AC/Cooler","Other Utility services"],
-  "TV": ["Samsung","LG","Toshiba","Videocon","Sony","Panasonic","Xiaomi Mi", "Onida"],
-  "Mobile": ["Vivo","Oppo","Micromax","Samsung","Apple","Xiaomi","Nokia","Lava"],
-  "Headphones": ["Boat","Bose","Sony","JBL","Sennheiser","Skullcandy"],
-  "Laptop": ["Lenovo","Dell","Asus","HP","Apple","Acer","Samsung","Toshiba"],
-  "Washing Machine": ["Videocon","LG","Whirlpool","Godrej","Onida","Samsung"],
-  "2-wheeler": ["Honda","Hero","Activa","Harley-Davidson"],
-  "4-wheeler": ["Toyota","Maruti","Hyundai"],
-  "Camera": ["Nikon","Sony","Erricson"],
-  "Fan/AC/Cooler": ["LG","Daikin","Usha","Bajaj"],
-  "Other Utility Service": ["Urban Clap"]
 };
 
+
+// ***********************************Element selector for category and brand search bar********************************
 abc= document.getElementById("cat");
 def= document.getElementById("brand")
-var sel;
-var selBrand;
+
+var sel;    //Selected category
+var selBrand;  //Selected Brand
+var clickId;  //ID of clicked buttons
+
+
+// *****************************************Element selector for icon buttons*******************************************
+var ghi= document.querySelectorAll(".icon-btn");
+var loop= ghi.length;
+
+
+//**********************************************Creating menu of category***********************************************
 for(var i=0;i<categor["categories"].length;i++)
 {
   abc.innerHTML += "<option>"+categor["categories"][i]+"</option>";
 }
-var ghi= document.querySelectorAll(".icon-btn");
-var loop= ghi.length;
+
+
+//*******************************************Adding click on icon buttons************************************************
 for(var i=0;i<loop;i++)
 {
   ghi[i].addEventListener("click",handleClick);
 }
 
-var clickId;
+
+//*******************************Function to add category list after a button is clicked**********************************
 function handleClick(){
   clickId= this.id;
   abc.innerHTML="<option>"+clickId+"</option>";
@@ -45,41 +49,59 @@ function handleClick(){
   {
     abc.innerHTML += "<option>"+categor["categories"][i]+"</option>";
   }
-
   categ(0);
 }
+
+
+//********************************Function to add option list as per selected category************************************
 var a;
 function categ(a){
   if(a===1)
   {
-      sel= abc.options[abc.selectedIndex].text;
+      sel= abc.options[abc.selectedIndex].text;   // Get the value of selected category
   }
   else
   {
     sel=clickId;
   }
   def.innerHTML="<option>Select Brand</option>";
-  for(var i=0;i<categor[sel].length;i++)
-  {
-    def.innerHTML += "<option>"+categor[sel][i]+"</option>";
-  }
-
+  const rootRef = firebase.database().ref(sel);
+   rootRef.on("value", function(data){
+       var mem= data.val();
+       var keys = Object.keys(mem);
+       var count =0;
+      for(var i=0;i<keys.length;i++)
+      {
+        var k=keys[i];
+        def.innerHTML += "<option>"+mem[k].brand+"</option>";
+      }
+    });  // Altering option list
 }
 
+
+//*****************************************Function to get value of selected brand****************************************
 function brand()
 {
   selBrand= def.options[def.selectedIndex].text;
 }
 
-var clik= document.getElementById("sbmt");
+
+ //*************************************************To tranfer data from one page to other*****************************
+var clik= document.getElementById("sbmt");       //Checking Click
 clik.addEventListener("click", function(){
-  localStorage.setItem("category", sel);
+  localStorage.setItem("category", sel);         //Transferring data
   localStorage.setItem("brands", selBrand);
-  window.document.location = "./page.html";
+  window.document.location = "./page.html";       //Connecting Second page
 });
+
 //Change
 document.getElementById("hide").style.display="none";
-$("button").on("click",function(){
+$("#feed").on("click",function(){
+  document.getElementById("feed").style.display="none";
+  document.getElementById("hide").style.display="block";
+  
+});
+$("#redir").on("click",function(){
   document.getElementById("feed").style.display="none";
   document.getElementById("hide").style.display="block";
 });
